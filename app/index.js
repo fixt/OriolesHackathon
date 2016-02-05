ad5c3c746f8d2b85920ad6637b7da11da4a5adc6
Christopher Garvis <cgarvis@gmail.com>
2016-02-05 14:26:20 -0500

    Remove sentry error catching

--- app/index.js
+++ app/index.js
@@ -22,7 +22,3 @@ const node = (
 );
 
 ReactDOM.render(node, target);
-
-if (process.env.NODE_ENV === 'production') {
-  Raven.config('https://82092c123a564dc08f5ede92d4f3c0b0@app.getsentry.com/62290').install();
-}
