a1ff05ae944fcf7d70c2f2408b5f71e0ebbfe6df
Christopher Garvis <cgarvis@gmail.com>
2016-02-05 12:46:02 -0500

    Add the reset of the pitch fields

--- server/db.js
+++ server/db.js
@@ -23,7 +23,109 @@ const Pitch = Conn.define('pitch', {
   sv_pitch_id: {
     type: Sequelize.STRING,
     primaryKey: true,
-  }
+  },
+  at_bat_number: {
+    type: Sequelize.INTEGER,
+  },
+  pitch_number: {
+    type: Sequelize.INTEGER,
+  },
+  inning: {
+    type: Sequelize.INTEGER,
+  },
+  top_inning_sw: {
+    type: Sequelize.STRING,
+  },
+  event_type: {
+    type: Sequelize.STRING,
+  },
+  event_result: {
+    type: Sequelize.STRING,
+  },
+  pre_balls: {
+    type: Sequelize.INTEGER,
+  },
+  pre_strikes: {
+    type: Sequelize.INTEGER,
+  },
+  pre_outs: {
+    type: Sequelize.INTEGER,
+  },
+  batter_id: {
+    type: Sequelize.INTEGER,
+  },
+  bat_side: {
+    type: Sequelize.STRING,
+  },
+  pitcher_id: {
+    type: Sequelize.INTEGER,
+  },
+  'throws': {
+    type: Sequelize.STRING,
+  },
+  initial_speed: {
+    type: Sequelize.DECIMAL,
+  },
+  plate_speed: {
+    type: Sequelize.DECIMAL,
+  },
+  pitch_type: {
+    type: Sequelize.STRING,
+  },
+  break_x: {
+    type: Sequelize.DECIMAL,
+  },
+  break_z: {
+    type: Sequelize.DECIMAL,
+  },
+  plate_x: {
+    type: Sequelize.DECIMAL,
+  },
+  plate_z: {
+    type: Sequelize.DECIMAL,
+  },
+  init_pos_x: {
+    type: Sequelize.DECIMAL,
+  },
+  init_pos_y: {
+    type: Sequelize.DECIMAL,
+  },
+  init_pos_z: {
+    type: Sequelize.DECIMAL,
+  },
+  init_vel_x: {
+    type: Sequelize.DECIMAL,
+  },
+  init_vel_y: {
+    type: Sequelize.DECIMAL,
+  },
+  init_vel_z: {
+    type: Sequelize.DECIMAL,
+  },
+  init_accel_x: {
+    type: Sequelize.DECIMAL,
+  },
+  init_accel_y: {
+    type: Sequelize.DECIMAL,
+  },
+  init_accel_z: {
+    type: Sequelize.DECIMAL,
+  },
+  hittrajectory: {
+    type: Sequelize.STRING,
+  },
+  playbyplay: {
+    type: Sequelize.STRING,
+  },
+  runneron1st_id: {
+    type: Sequelize.INTEGER,
+  },
+  runneron2nd_id: {
+    type: Sequelize.INTEGER,
+  },
+  runneron3rd_id: {
+    type: Sequelize.INTEGER,
+  },
 }, {
   timestamps: false,
 })
