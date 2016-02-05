a83e12d8e102ce1d43c939ec39442cbc48533dd1
Eric Sauter <esauter5@gmail.com>
2016-02-05 15:53:27 -0500

    Wire up game stats

--- app/components/on-base.js
+++ app/components/on-base.js
@@ -7,18 +7,19 @@ import wrapper from './wrapper';
 
 class OnBase extends Component {
   render() {
+    let { inning, runneron1st_id, runneron2nd_id, runneron3rd_id, top_inning_sw } = this.props.currentPitch;
+
     return (
       <Box>
-        <div style={{flex: 1, justifyContent: 'center', textAlign: 'center'}}>Bot 1st</div>
+        <div style={{flex: 1, justifyContent: 'center', textAlign: 'center'}}>{top_inning_sw ? 'Top' : 'Bot'} {inning}</div>
         <div style={{display: 'flex', flex: 1, justifyContent: 'center', textAlign: 'center', margin: 25, fontSize: '2em'}}>
           <div style={{height: '30px', transform: 'rotate(45deg)', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', width: '30px', border: '1px solid black'}}>
             <div style={{display: 'flex', flex:1, justifyContent: 'space-between'}}>
-              <div style={{height: '5px', width: '5px', backgroundColor: 'black', border: '1px solid black'}}></div>
-              <div style={{height: '5px', width: '5px', backgroundColor: 'black', border: '1px solid black'}}></div>
+              <div style={{height: '5px', width: '5px', backgroundColor: runneron2nd_id != null ? 'black' : 'white', border: '1px solid black'}}></div>
+              <div style={{height: '5px', width: '5px', backgroundColor: runneron2nd_id != null ? 'black' : 'white', border: '1px solid black'}}></div>
             </div>
             <div style={{display: 'flex', flex: 1, justifyContent: 'space-between', alignItems: 'flex-end'}}>
-              <div style={{height: '5px', width: '5px', backgroundColor: 'black', border: '1px solid black'}}></div>
-              <div style={{height: '5px', width: '5px', backgroundColor: 'black', border: '1px solid black'}}></div>
+              <div style={{height: '5px', width: '5px', backgroundColor: runneron3rd_id != null ? 'black' : 'white', border: '1px solid black'}}></div>
             </div>
           </div>
         </div>
