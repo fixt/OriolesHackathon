4fa858c9db675d2955247117af16445ced6e7c87
Christopher Garvis <cgarvis@gmail.com>
2016-02-05 10:22:24 -0500

    Babel/Webpack/ESLint

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var path = require('path');
+
+module.exports = {
+  entry: './app',
+  devtool: 'cheap-module-source-map',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.[hash].js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  resolveLoader: {
+    modulesDirectories: ['node_modules']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Fixt Orioles Hackathon',
+      template: './app/index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '\'' + process.env.NODE_ENV + '\''
+    })
+  ],
+  module: {
+    loaders: [
+      { test: /\.js?$/,
+        loaders: ['react-hot-loader', 'babel'],
+        exclude: /(node_modules|lib)/ },
+    ]
+  },
+  resolve: { fallback: path.join(__dirname, 'node_modules') },
+  resolveLoader: { fallback: path.join(__dirname, 'node_modules') }
+};
